c6a3cfcdaaadbccc4de35d5d376f295100adab32
Haley G Mull <haley.mull@gmail.com>
2020-05-24 11:53:03 -0400

    Getting and Cleaning Data Week 4 Project (Tidy data script, Averaged Data final csv, README.txt, and data dictionary (codebook))

---
+++ README.txt
@@ -0,0 +1,73 @@
+==================================================================
+Tidy Data Assignment: Getting and Cleaning Data - Coursera
+Version 1.0
+Note: Information about project is taken from the original Readme for those interested. 
+==================================================================
+Data Source
+
+Jorge L. Reyes-Ortiz, Davide Anguita, Alessandro Ghio, Luca Oneto.
+Smartlab - Non Linear Complex Systems Laboratory
+DITEN - Universit? degli Studi di Genova.
+Via Opera Pia 11A, I-16145, Genoa, Italy.
+activityrecognition@smartlab.ws
+www.smartlab.ws
+==================================================================
+Experiment Information: (Given)
+
+The experiments have been carried out with a group of 30 volunteers within an age bracket of 19-48 years. Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) wearing a smartphone (Samsung Galaxy S II) on the waist. Using its embedded accelerometer and gyroscope, we captured 3-axial linear acceleration and 3-axial angular velocity at a constant rate of 50Hz. The experiments have been video-recorded to label the data manually. The obtained dataset has been randomly partitioned into two sets, where 70% of the volunteers was selected for generating the training data and 30% the test data. 
+
+The sensor signals (accelerometer and gyroscope) were pre-processed by applying noise filters and then sampled in fixed-width sliding windows of 2.56 sec and 50% overlap (128 readings/window). The sensor acceleration signal, which has gravitational and body motion components, was separated using a Butterworth low-pass filter into body acceleration and gravity. The gravitational force is assumed to have only low frequency components, therefore a filter with 0.3 Hz cutoff frequency was used. From each window, a vector of features was obtained by calculating variables from the time and frequency domain. See 'features_info.txt' for more details. 
+
+For each record it is provided:
+======================================
+
+- Triaxial acceleration from the accelerometer (total acceleration) and the estimated body acceleration.
+- Triaxial Angular velocity from the gyroscope. 
+- A 561-feature vector with time and frequency domain variables. 
+- Its activity label. 
+- An identifier of the subject who carried out the experiment.
+
+The dataset includes the following files:
+=========================================
+
+- 'README.txt'
+
+- 'Data_Dictionary.txt': List of all variables and unit of observation (codebook)
+
+- 'run_analysis.R': Script to create full and averaged tidy data set
+
+- 'train/subject_train.txt': Each row identifies the subject who performed the activity for each window sample. Range from 1-30 (observation identifiers)
+
+- 'test/subject_test.txt': Each row identifies the subject who performed the activity for each window sample. Range from 1-30 (observation identifiers)
+
+- 'activity_labels.txt': Links the class labels with their activity name.
+
+- 'features.txt': Provides the variable name for each column in X_test.txt and X_train.txt 
+
+- 'train/X_train.txt': Training set (variable measurements)
+
+- 'train/y_train.txt': Training labels (activity)
+
+- 'test/X_test.txt': Test set (variable measurements)
+
+- 'test/y_test.txt': Test labels (activity)
+
+- "Averaged_Data.csv": Final tidy data set of average for each subject-activity combination's variables
+
+
+Notes: 
+======
+- Features are normalized and bounded within [-1,1].
+- Each feature vector is a row on the text file.
+
+For more information about this dataset contact: activityrecognition@smartlab.ws
+
+License:
+========
+Use of this dataset in publications must be acknowledged by referencing the following publication [1] 
+
+[1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
+
+This dataset is distributed AS-IS and no responsibility implied or explicit can be addressed to the authors or their institutions for its use or misuse. Any commercial use is prohibited.
+
+Jorge L. Reyes-Ortiz, Alessandro Ghio, Luca Oneto, Davide Anguita. November 2012.
---
+++ run_analysis.R
@@ -0,0 +1,127 @@
+library(tidyverse)
+library(plyr)
+
+## Steps 1-4
+#### Cleaning Training Data ####
+# Import Training Data and labels
+train1 <- read_delim("~/Google Drive/Coursera/datasciencecoursera/Data/train/subject_train.txt", " ", col_names = F)
+train2 <- read_delim("~/Google Drive/Coursera/datasciencecoursera/Data/train/X_train.txt", " ", col_names = F)
+train3 <- read_delim("~/Google Drive/Coursera/datasciencecoursera/Data/train/y_train.txt", " ", col_names = F)
+feat_labs <- read_delim("~/Google Drive/Coursera/datasciencecoursera/Data/features.txt", " ", col_names = F)
+act_labs <- read_delim("~/Google Drive/Coursera/datasciencecoursera/Data/activity_labels.txt", " ", col_names = F)
+
+# Rename variables from train2
+old_names <- names(train2)
+new_names <- feat_labs$X2 
+for (i in 1:length(train2)){
+  names(train2)[names(train2) == old_names[i]] = new_names[i]
+}
+
+# Rename variables from train3 and turn into vector with proper column name (activity)
+act_labels <- act_labs$X2
+train3 <- as.character(train3$X1)
+names(train3) = "Activity"
+for(i in 1:length(train3)){
+  if(train3[i] == 1){
+    train3[i] = act_labels[1]
+  }else if(train3[i] == 2){
+    train3[i] = act_labels[2]
+  }else if (train3[i] == 3){
+    train3[i] = act_labels[3]
+  }else if (train3[i] == 4){
+    train3[i] = act_labels[4]
+  }else if (train3[i] == 5){
+    train3[i] = act_labels[5]
+  }else if (train3[i] == 6){
+    train3[i] = act_labels[6]
+  }
+}
+train3 <- as.data.frame(train3)
+names(train3) = "Activity"
+
+# Rename train1 and turn into data.frame 
+train1 <- as.data.frame(train1)
+names(train1) = "Subject"
+
+# Combine Subject, Activity, and Measurements 
+training <- cbind.data.frame(train1, train3, train2)
+
+#Extract all columns with mean and standard deviations 
+means_train <- grepl("\\bmean\\b", names(training))
+mean_cols <- which(means_train[] == T)
+sd_train <- grepl("\\bstd\\b", names(training))
+sd_cols <- which(sd_train[] == T)
+
+#Creating Training Data Frame with correct columns, subject column, activity column, and add group identifier 
+training <- training[, c(1, 2, mean_cols, sd_cols)]
+training$Group <- "Training Sample"
+
+#### Cleaning Test Data ####
+
+# Test Data 
+test1 <- read_delim("~/Google Drive/Coursera/datasciencecoursera/Data/test/subject_test.txt", " ", col_names = F)
+test2 <- read_delim("~/Google Drive/Coursera/datasciencecoursera/Data/test/X_test.txt", " ", col_names = F)
+test3 <- read_delim("~/Google Drive/Coursera/datasciencecoursera/Data/test/y_test.txt", " ", col_names = F)
+
+# Rename variables from test2
+old_names <- names(test2)
+new_names <- feat_labs$X2 
+for (i in 1:length(test2)){
+  names(test2)[names(test2) == old_names[i]] = new_names[i]
+}
+
+# Rename variables from test3 and turn into vector with proper column name (activity)
+act_labels <- act_labs$X2
+test3 <- as.character(test3$X1)
+names(test3) = "Activity"
+for(i in 1:length(test3)){
+  if(test3[i] == 1){
+    test3[i] = act_labels[1]
+  }else if(test3[i] == 2){
+    test3[i] = act_labels[2]
+  }else if (test3[i] == 3){
+    test3[i] = act_labels[3]
+  }else if (test3[i] == 4){
+    test3[i] = act_labels[4]
+  }else if (test3[i] == 5){
+    test3[i] = act_labels[5]
+  }else if (test3[i] == 6){
+    test3[i] = act_labels[6]
+  }
+}
+test3 <- as.data.frame(test3)
+names(test3) = "Activity"
+
+# Rename test1 and turn into data.frame 
+test1 <- as.data.frame(test1)
+names(test1) = "Subject"
+
+# Combine Subject, Activity, and Measurements 
+testing <- cbind.data.frame(test1, test3, test2)
+
+#Extract all columns with mean and standard deviations 
+means_test <- grepl("\\bmean\\b", names(testing))
+mean_cols <- which(means_test[] == T)
+sd_test <- grepl("\\bstd\\b", names(testing))
+sd_cols <- which(sd_test[] == T)
+
+#Creating Testing Data Frame with correct columns, subject column, activity column, and add group identifier 
+testing <- testing[, c(1, 2, mean_cols, sd_cols)]
+testing$Group <- "Testing Sample"
+
+#### Creating Full Data Set ####
+data <- rbind(training, testing)
+
+
+#Step 5: From the data set in step 4, creates a second, independent tidy data set with the 
+# average of each variable for each activity and each subject.
+#Coerce columns to be numeric rather than character 
+data[, 3:68] <- sapply(data[, 3:68], as.numeric)
+str(data)
+
+#Sumarise into final data set 
+avg_data <- data %>%
+  dplyr::group_by(Subject, Activity) %>% 
+  dplyr::summarise_all(funs(mean))
+
+write.csv(avg_data, "Averaged_Data.csv")
